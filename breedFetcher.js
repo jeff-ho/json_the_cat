0409c6dc0f66fee1bea114abf4ea82712c9c54c5
Jeffrey Ho <jeff.ho@hotmail.com>
2022-08-04 20:31:48 +0000

    refactored breed fetcher

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,23 +1,21 @@
 let request = require("request");
 const arg = process.argv;
 
-const breedFetcher = () => {
-  const breed = arg[2];
+const fetchBreedDescription = function (breed, callback) {
   let url = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
   request(url, (error, response, body) => {
     if (error) {
-      console.log("error:", error);
-      return;
+      callback(error);
     }
 
     const data = JSON.parse(body);
+
     if (data.length === 0) {
       console.log("Your requested breed is not found!");
-      return;
     } else {
-      console.log(data[0].description);
+      callback(error, data[0].description);
     }
   });
 };
 
-breedFetcher();
+module.exports = { fetchBreedDescription };
